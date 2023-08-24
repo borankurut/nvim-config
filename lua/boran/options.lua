275ef39c3b9f302b329a92036549b270a23c4f78
Boran Kurut <borankurut@gmail.com>
2023-08-24 03:52:41 +0300

    options created

---
+++ lua/boran/options.lua
@@ -0,0 +1,32 @@
+-- :help options
+vim.opt.backup = false
+vim.opt.clipboard = "unnamedplus" --for ctrl-c and v to work.
+vim.opt.cmdheight = 2
+vim.opt.completeopt = { "menuone", "noselect" }
+vim.opt.conceallevel = 0
+vim.opt.fileencoding = "utf-8"
+vim.opt.ignorecase = true
+vim.opt.showtabline = 2
+vim.opt.smartcase = true
+vim.opt.smartindent = true
+vim.opt.splitright = true
+vim.opt.termguicolors = true
+vim.opt.undofile = true -- records history, don't do frequent changes to large files
+vim.opt.updatetime = 600
+
+vim.opt.softtabstop=4
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.wrap = false
+
+vim.opt.cursorline = true
+--set the hexadecimal color for line highlighting
+vim.cmd [[
+    hi CursorLine ctermbg=20 guibg=#1f1f1f
+]]
+
+

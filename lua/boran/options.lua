3d5a0971ce8524b2d6e75461c111f94648c48068
Boran Kurut <borankurut@gmail.com>
2023-08-31 02:48:18 +0300

    up

--- lua/boran/options.lua
+++ lua/boran/options.lua
@@ -1,4 +1,20 @@
 -- :help options
+
+--opacity and some ruler stuf
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalF", { bg = "none" })
+vim.opt.laststatus = 2
+vim.opt.ruler = false
+vim.opt.number = false
+
+vim.cmd [[
+  highlight StatusLine guibg=NONE ctermbg=NONE gui=NONE cterm=NONE
+  highlight StatusLineNC guibg=NONE ctermbg=NONE gui=NONE cterm=NONE
+]]
+
+--reduce sign column size
+vim.o.signcolumn = "number"
+
 vim.opt.backup = false
 vim.opt.clipboard = "unnamedplus" --for ctrl-c and v to work.
 vim.opt.cmdheight = 2
@@ -12,6 +28,7 @@ vim.opt.smartindent = true
 vim.opt.splitright = true
 vim.opt.termguicolors = true
 vim.opt.undofile = true -- records history, don't do frequent changes to large files
+
 vim.opt.updatetime = 600
 
 vim.opt.softtabstop=4

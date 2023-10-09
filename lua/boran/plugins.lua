4386d21407ffbe5f74449e80a92415e88e0d6c04
boran kurut <borankurut@gmail.com>
2023-10-09 04:24:24 +0300

    glsl syntax highlighting

--- lua/boran/plugins.lua
+++ lua/boran/plugins.lua
@@ -124,6 +124,9 @@ return packer.startup(function(use)
 	--harpoon
 	use { 'ThePrimeagen/harpoon' }
 
+	--glsl syntax
+	use { 'tikhomirov/vim-glsl' }
+
 	-- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
 	if PACKER_BOOTSTRAP then

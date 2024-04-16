13481d121d198c5fd149ac522ef98874689c149a
borankurut <borankurut@gmail.com>
2024-04-16 12:29:46 +0300

    illuminate bug fix

--- lua/boran/toggleterm.lua
+++ lua/boran/toggleterm.lua
@@ -35,7 +35,7 @@ toggleterm.setup({
 	persist_size = true,
 	direction = "horizontal",
 	close_on_exit = true,
-	shell = vim.o.shell,
+	shell = "pwsh",
 	float_opts = {
 		border = "curved",
 		winblend = 0,
